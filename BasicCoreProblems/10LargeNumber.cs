6fe5cfaebc55ce08a290ce5ed1ccd1206d063451
nikita5677 <67727961+nikita5677@users.noreply.github.com>
2022-11-24 08:45:52 +0530

    Add project files

--- BasicCoreProblems/10LargeNumber.cs
+++ BasicCoreProblems/10LargeNumber.cs
@@ -17,22 +17,19 @@ namespace BasicCoreProblems
             num2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the num3");
             num3 = Convert.ToInt32(Console.ReadLine());
-            if (num1 > num2)
+            if (num1 > num2 && num1 > num3)
             {
-                if (num1 > num3)
-                {
-                    Console.Write("Number one is the largest!\n");
-                }
-                else
-                {
-                    Console.Write("Number three is the largest!\n");
-                }
+                Console.WriteLine("Number one is greater");
+                
             }
-            else if (num2 > num3)
-                Console.Write("Number two is the largest!\n");
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("number two is greater");
+            }
             else
-                Console.Write("Number three is the largest!\n");
+            {
+                Console.WriteLine("Number three is greater");
+            }
         }
-
     }
 }
